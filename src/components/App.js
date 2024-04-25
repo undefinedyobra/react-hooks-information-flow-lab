35cf449dde53a3c43bad4733c52c186091cd1b96
undefinedyobra <brianmelly008@gmail.com>
2024-04-25 16:54:13 +0300

    Complete Lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,25 @@
 import React, { useState } from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
+import Header from "./Header";
+import Filter from "./Filter";
 
 function App() {
   const [isDarkMode, setIsDarkMode] = useState(false);
+  const [selectedCategory, setSelectedCategory] = useState("all"); // Initialize selectedCategory state
 
   function onDarkModeClick() {
     setIsDarkMode((isDarkMode) => !isDarkMode);
   }
+
+  function onCategoryChange(e) {
+    setSelectedCategory(e.target.value);
+  }
+
+  const filteredItems =
+    selectedCategory === "all"
+      ? itemData
+      : itemData.filter((item) => item.category === selectedCategory);
 
   return (
     <div className={"App " + (isDarkMode ? "dark" : "light")}>
@@ -17,7 +29,8 @@ function App() {
           {isDarkMode ? "Dark" : "Light"} Mode
         </button>
       </header>
-      <ShoppingList items={itemData} />
+      <Filter onCategoryChange={onCategoryChange} />
+      <ShoppingList items={filteredItems} />
     </div>
   );
 }
---
+++ src/components/Filter.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Filter({ onCategoryChange }) {
+  return (
+    <div>
+      <label>Filter by Category:</label>
+      <select onChange={onCategoryChange}>
+        <option value="all">All</option>
+        <option value="food">Food</option>
+        <option value="clothing">Clothing</option>
+
+      </select>
+    </div>
+  );
+}
+
+export default Filter;
